59d5573a10c754a226399699f9630531fd87fe37
choisuill <gongdal5@naver.com>
2021-08-18 20:38:40 +0900

    env

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
 import express from 'express';
 const indexrouter = require('../src/routers/indexRouter')
 const connect = require('./db/db')
+const cors = require('cors')
 import dotenv from 'dotenv'
 dotenv.config()
 
 const app = express();
 
+app.use(cors())
 app.set('port', process.env.SERVER_PORT);
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/', indexrouter)
-app.use((req: any, res: any) => {
-  res.header("Access-Control-Allow-Origin", "*")
-})
+
 
 connect()
 
