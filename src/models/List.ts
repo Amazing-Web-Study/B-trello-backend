dcc5fca6a6e74d9a5072cce463722d791e194ba0
choi suill <gongdal5@naver.com>
2021-09-05 19:12:54 +0900

    get filtering, delete, put

--- src/models/List.ts
+++ src/models/List.ts
@@ -1,17 +1,15 @@
 import mongoose from 'mongoose'
 
 const ListSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId },
+    user_id: { type: String },
     // user_id 타입은 _id(ObjectId) 타입이다.
     title: { type: String, required: true },
     date: { type: Date }
-},{
-    versionKey: false
-    })
+})
 
 const List = mongoose.model('List', ListSchema)
 // const list1 = new List({
-//     user_id: new mongoose.Types.ObjectId(),
+//     user_id: '612203c33263024508a68730',
 //     title: '테스트아아아아',
 //     date: new Date()
 // })
--- src/routers/list/ListRouter.ts
+++ src/routers/list/ListRouter.ts
@@ -8,24 +8,30 @@ const listController = require('../../controllers/ListController')
 // router.get('/', verifyToken, listController.getAll)
 // router.get('/', listController.getAll)
 
-router.get("/", (req: Request, res: Response):void => {
+router.get("/:id", (req: Request, res: Response):void => {
     List.find((err:any, lists: any):void => {
+        const {id} = req.params
+        console.log(lists)
         if (err) res.status(500).send({err: '백엔드 개발자에게 카톡주세요!(db find 오류)'})
-        res.json(lists)
+        const filtering_list = lists.filter((a:any) => {
+            return a.user_id === id
+        })
+        res.json(filtering_list)
     })
 })
 
 router.post("/", (req:Request, res: Response):void => {
     let userId = req.body.user_id
     if (!req.body.user_id) {
-        userId = new mongoose.Types.ObjectId()
+        res.json({error: 'user_id가 빠졌습니다!'})
     }
-    const list1 = new List({
+    const list = new List({
         user_id: userId,
         title: req.body.title,
         date: new Date
     })
-    list1.save()
+
+    list.save()
         .then(() => {
             res.json({result: 1})
         })
@@ -33,6 +39,31 @@ router.post("/", (req:Request, res: Response):void => {
             console.log(err)
             res.json({result: 0})
         })
+})
+
+router.delete('/:id', async (req: Request, res: Response) => {
+    const {id} = req.params
+    try{
+        await List.findByIdAndDelete(id).exec()
+        return res.status(204).end()
+    } catch(err) {
+        return res.status(500).json({error: err})
+    }
+})
+
+router.put('/:id', (req: Request, res: Response) => {
+    const {id} = req.params
+    List.findById(id, (err:any, list:any) => {
+        if (err) return res.status(500).json({error: err})
+        if(!List) return res.status(400).json({error: '아이디에 맞는 리스트가 없음!'})
+        if (req.body.title) list.title = req.body.title
+        console.log(list)
+
+        list.save((err:any) => {
+            if (err) res.status(500).json({error: '저장할때 에러남'})
+            res.json({message: 'list update'})
+        })
+    })
 })
 
 
